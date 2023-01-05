4df0ff2e35e0a77fd05108580acfe82a01b02b5b
rojan-abolhasani <58859111+rojan-abolhasani@users.noreply.github.com>
2023-01-05 20:36:20 +0330

    Update script.js

--- script/script.js
+++ script/script.js
@@ -12,7 +12,7 @@ const alert_text = document.getElementById("alertText")
 localStorage.clear()
 
 API_KEY = ""
-API_ENDPOINT = "https://api.gisadasdsadadasdasdasdsadsathub.com/users/"
+API_ENDPOINT = "https://api.github.com/users/"
 
 // this function is used to update the Profile info according to the request response
 function update(data) {
@@ -33,6 +33,64 @@ function update(data) {
     console.log(pic)
 }
 
+
+// sending a request, receiving the response and handling the possible errors
+function process(user, update) {
+    // get data from the local storage  (key: user)
+    let res = localStorage.getItem(user)
+
+    // If it isn't in the local storage, send a fetch request
+    if (res == null) {
+        fetch(API_ENDPOINT + user)   // path to the resource we want to fetch (github), default method: GET
+            .then((res) => {
+                /*if(!Response.ok) // in try-catch
+                {
+                    throw new Error("Failed to fetch the data : ${response.status}")
+                }*/
+
+                if (res.status == 200)  // the request has succeeded
+                {
+                    return res.json()   //return the response in json format
+                }
+                else if (res.status == 404)   // username not found
+                {
+                    return Promise.reject(404)
+                }
+            }).then((data) => {
+                // save data to local storage (key:user, value: a JSON string)
+                localStorage.setItem(user, JSON.stringify(data))
+
+                // use the update function to show the data on the website   
+                update(data)
+                // network error
+            })
+            .catch((err) => {
+                if (err == 404) {
+                    alert_text.innerHTML = "Username not Found"
+                }
+                else {
+                    alert_text.innerHTML = "Something went wrong!" + err
+                }
+                alertObj.style.opacity = 1
+                setTimeout(() => {
+                    alertObj.style.opacity = 0
+                }, 2500)
+            })
+    }
+    // the data is already in the local storage
+    else {
+        res = JSON.parse(res) // convert to a JavaScript object
+        update(res)
+    }
+}
+
+
+// setting up a function that will be called whenever the user clicks on the submit button
+button.addEventListener("click", (event) => {
+    event.preventDefault()
+    user = text.value
+    process(user, update)
+})
 
 // sending a request, receiving the response and handling the possible errors
 function process(user, update) {
